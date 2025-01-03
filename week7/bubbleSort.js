1bfc5ad8d72a98a882654e54c79a778bf435adc4
lumpen <swk5373@gmail.com>
2025-01-04 00:22:10 +0900

    week7

--- week7/bubbleSort.js
+++ week7/bubbleSort.js
@@ -2,14 +2,16 @@ const arr = [5, 1, 4, 2, 8]
 
 
 const bubbleSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j=i; j<arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        [arr[i], arr[j]] = [arr[j], arr[i]]
+  const { length } = arr;
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = 0; j < length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
-  return arr
-}
+  return arr;
+};
+
 
 console.log(bubbleSort(arr))
